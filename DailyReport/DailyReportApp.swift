561e4e418a2985bf852d02ef56444f38d92fcd26
kxx <132kxx@gmail.com>
2022-12-24 22:32:39 +0900

    Initial Commit

---
+++ DailyReport/DailyReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyReportApp.swift
+//  DailyReport
+//
+//  Created by kxx: on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
