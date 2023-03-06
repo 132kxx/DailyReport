99f95e2f95243f68f295773927acc7f502e87de4
kxx <132kxx@gmail.com>
2023-03-06 09:36:02 +0900

    component

--- DailyReport/View/tableView.swift
+++ DailyReport/View/tableView.swift
@@ -27,64 +27,11 @@ struct TableView: View {
                     }
                     //sheet
                     .sheet(isPresented: $showSheet) {
-                        VStack(spacing: 15) {
-                            // current time
-                            Text("8")
-                                .fontWeight(.bold)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top)
-                                .onAppear {
-//                                    hour = task.hour + "시"
-//                                    presentText = ""
-                                }
-
-                            // textfield
-                            ZStack {
-                                TextField("내용", text: $presentText)
-                                    .textFieldStyle(.roundedBorder)
-                                
-                                // delete button
-                                Button {
-                                    presentText = ""
-                                } label: {
-                                    Text("delete")
-                                        .foregroundColor(presentText == "" ? .clear : .white)
-                                        .padding(5)
-                                        .background {
-                                            RoundedRectangle(cornerRadius: 8)
-                                                .foregroundColor( presentText == "" ? .clear : .red )
-                                        }
-
-                                }
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .padding(.trailing, 5)
-                            }
-
-                            //submit button
-                            Button {
-                                presentText = ""
-                                showSheet.toggle()
-                            } label: {
-                                Text("Submit")
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                            }
-                            .background {
-                                Capsule()
-                                    .foregroundColor(.blue)
-                            }
-                        }
-                        .padding(.horizontal)
-                        .presentationDetents([.height(150)])
-                        .presentationDragIndicator(.visible)
+                        sheetView()
                     }
-                }
-            
-            
-
             }
         }
+    }
 }
 
 struct TableView_Previews: PreviewProvider {
@@ -92,4 +39,56 @@ struct TableView_Previews: PreviewProvider {
         MainView()
             .environmentObject(ViewModel())
     }
+}
+
+// MARK: COMPONENT
+
+struct sheetView: some View {
+    VStack(spacing: 15) {
+        // current time
+        Text("8")
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top)
+
+        // textfield
+        ZStack {
+            TextField("내용", text: $presentText)
+                .textFieldStyle(.roundedBorder)
+            
+            // delete button
+            Button {
+                presentText = ""
+            } label: {
+                Text("delete")
+                    .foregroundColor(presentText == "" ? .clear : .white)
+                    .padding(5)
+                    .background {
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor( presentText == "" ? .clear : .red )
+                    }
+
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.trailing, 5)
+        }
+        
+        //submit button
+        Button {
+            presentText = ""
+            showSheet.toggle()
+        } label: {
+            Text("Submit")
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .center)
+        }
+        .background {
+            Capsule()
+                .foregroundColor(.blue)
+        }
+    }
+    .padding(.horizontal)
+    .presentationDetents([.height(150)])
+    .presentationDragIndicator(.visible)
 }
